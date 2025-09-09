1643865a647360cb6fa55d45ee0f3d6f4cd5b683
NotArt <noterisarthur42@gmail.com>
2025-09-09 04:13:51 +0200

    Win by capturing 5 pairs
    Put check_bounds function outside Board class

--- board.py
+++ board.py
@@ -2,9 +2,18 @@ import pyglet
 from const import *
 from button import CustomButton
 
+def check_bounds(x, y, dx, dy, length):
+	return (x + dx * (length - 1) < BOARD_SIZE
+		and y + dy * (length - 1) < BOARD_SIZE
+		and x + dx * (length - 1) >= 0
+		and y + dy * (length - 1) >= 0)
+
 class Board():
 	def __init__(self):
 		self.turn = BLACK
+		self.black_pairs_captured = 0
+		self.white_pairs_captured = 0
+
 		self.batch = pyglet.graphics.Batch()
 
 		self.board_lines = [] # This is needed to keep every line in memory
@@ -25,12 +34,9 @@ class Board():
 
 
 	def change_turn(self):
-		# if (self.check_horizontal_possible_capture(0, 0, self.turn)):
-		# 	print("ouioui")
-		
 		# Checks for victory before changing turn (player)
 		winner = self.check_victory()
-		if winner is not False:
+		if winner is not False :
 			print(f'{"White" if (winner == WHITE) else "Black"} player wins !')
 			pyglet.app.exit()
 			return
@@ -40,21 +46,22 @@ class Board():
 		for row in self.buttons:
 			for button in row:
 				button.change_turn_img()
-		
-		for x in range(BOARD_SIZE):
-			for y in range(BOARD_SIZE):
-				if self.is_capturable(x, y):
-					print(f'{x, y} is capturable !')
+
 
 	def check_victory(self):
+		if self.black_pairs_captured >= 5 :
+			return BLACK
+		if self.white_pairs_captured >= 5 :
+			return WHITE
+		
 		prev_color = WHITE if (self.turn == BLACK) else BLACK # This is needed to keep win order, in case both players make a line of five
 		for player in [prev_color, self.turn]:
 			for x in range(BOARD_SIZE):
 				for y in range(BOARD_SIZE):
 					for dx, dy in DIRECTIONS:
 						if self.check_line(x, y, dx, dy, player):
-							if player != self.turn: # As the winning player is about to play, there's no way for him to lose his five in a row.
-								return player # The winning player
+							if player != self.turn : # As the winning player is about to play, there's no way for him to lose his five in a row.
+								return player
 
 							# Check if the winning line can be broken by a capture
 							for i in range(WIN_LINE):
@@ -62,36 +69,32 @@ class Board():
 									self.buttons[y + (dy * i)][x + (dx * i)].state = NOT_SELECTED
 									ineluctable_win = self.check_victory()
 									self.buttons[y + (dy * i)][x + (dx * i)].state = player
-									if (not ineluctable_win):
+									if not ineluctable_win :
 										return False
 
-							return player # The winning player
+							return player
 		return False
-	
-	def check_bounds(self, x, y, dx, dy, length):
-		return (x + dx * (length - 1) < BOARD_SIZE
-			and y + dy * (length - 1) < BOARD_SIZE
-			and x + dx * (length - 1) >= 0
-			and y + dy * (length - 1) >= 0)
+
 
 	def check_line(self, x, y, dx, dy, player):
-		if not self.check_bounds(x, y, dx, dy, WIN_LINE):
+		if not check_bounds(x, y, dx, dy, WIN_LINE):
 			return False
 
 		for i in range(WIN_LINE):
-			if self.buttons[y + i * dy][x + i * dx].state != player:
+			if self.buttons[y + i * dy][x + i * dx].state != player :
 				return False
 		return True
+
 
 	def is_capturable(self, x, y):
 		color = self.buttons[y][x].state
 		opp_color = WHITE if (color == BLACK) else BLACK
-		if color == NOT_SELECTED:
+		if color == NOT_SELECTED :
 			return False
 		
 		for dx, dy in DIRECTIONS:
 			for dir in [1, -1]: # Double the directions, to have every possible one
-				if (not self.check_bounds(x, y, (dx * dir), (dy * dir), 3) or not self.check_bounds(x, y, (dx * -dir), (dy * -dir), 2)):
+				if (not check_bounds(x, y, (dx * dir), (dy * dir), 3) or not check_bounds(x, y, (dx * -dir), (dy * -dir), 2)):
 					continue
 
 				if (self.buttons[y + (dy * dir)][x + (dx * dir)].state == color):
@@ -106,10 +109,10 @@ class Board():
 	def is_capture_move(self, x, y, dx, dy):
 		color = self.buttons[y][x].state
 		opp_color = WHITE if (color == BLACK) else BLACK
-		if color == NOT_SELECTED:
+		if color == NOT_SELECTED :
 			return False
 		
-		if not self.check_bounds(x, y, dx, dy, 4):
+		if not check_bounds(x, y, dx, dy, 4):
 			return False
 		
 		if (self.buttons[y + dy * 1][x + dx * 1].state == opp_color
@@ -125,3 +128,8 @@ class Board():
 				if self.is_capture_move(x, y, (dx * dir), (dy * dir)):
 					self.buttons[y + (dy * dir * 1)][x + (dx * dir * 1)].reset()
 					self.buttons[y + (dy * dir * 2)][x + (dx * dir * 2)].reset()
+	
+					if self.turn == BLACK :
+						self.black_pairs_captured += 1
+					else:
+						self.white_pairs_captured += 1
